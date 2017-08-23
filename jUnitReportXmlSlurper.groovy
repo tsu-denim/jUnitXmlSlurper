e13cc46830ab73e1f2bd7d2b5099379e906cf686
Kurt Waechter <kurt.waechter@blackboard.com>
2017-08-23 11:04:08 -0400

    Initial Commit

---
+++ jUnitReportXmlSlurper.groovy
@@ -0,0 +1,197 @@
+import groovy.io.FileType
+import junit.framework.TestCase
+import java.util.stream.Collectors
+
+def getTestReportSummary(String resultPath){
+    return new JunitTestReportSummary(resultPath)
+}
+
+public class JunitTestReportSummary{
+    public final int testFailureCount;
+    public final int testPassCount;
+    public final int testSkipCount;
+    public final List<TestExecution> testExecutionList;
+    private final String testReportPath;
+
+    //Prevent compiler from creating default zero arg constructor
+    private JunitTestReportSummary() {
+        assert false: "Zero argument constructor should not be called, object creation for this class requires a file path.";
+    }
+
+    public JunitTestReportSummary(String jUnitReportDirectoryPath) {
+        if (riskyStringNotEmpty(jUnitReportDirectoryPath)) {
+            this.testReportPath = jUnitReportDirectoryPath;
+        } else {
+            assert false: "Path to jUnit report directory was null or empty."
+        }
+
+        this.testExecutionList = getTestExecutionList();
+        long failCount = testExecutionList.stream().filter({t -> t.isFailed}).count();
+        long passCount = testExecutionList.stream().filter({t -> t.isPassed}).count();
+        long skipCount = testExecutionList.stream().filter({t -> t.isSkipped}).count();
+
+        this.testFailureCount = failCount.intValue();
+        this.testPassCount = passCount.intValue();
+        this.testSkipCount = skipCount.intValue();
+
+    }
+
+    //Checks a string for variations of being 'empty'
+    private riskyStringNotEmpty(String riskyString) {
+        boolean isNotEmpty = riskyString != null && !riskyString.trim().isEmpty();
+
+        return isNotEmpty;
+    }
+
+    private List<TestExecution> getTestExecutionList(){
+        return new JunitXmlReader(this.testReportPath).getTestExecutions();
+    }
+
+    public List<TestExecution> getFailedTestList() {
+        return testExecutionList.stream().filter({t -> t.isFailed}).collect(Collectors.toList());
+    }
+
+    public List<TestExecution> getPassedTestList() {
+        return testExecutionList.stream().filter({t -> t.isPassed}).collect(Collectors.toList());
+    }
+
+    public List<TestExecution> getSkippedTestList() {
+        return testExecutionList.stream().filter({t -> t.isSkipped}).collect(Collectors.toList());
+    }
+
+    public List<String> getFailedTestReportPaths(){
+        return testExecutionList.stream().filter({t -> t.isFailed}).map({t -> t.pathToJunitReport}).distinct().collect(Collectors.toList());
+    }
+
+}
+
+public class JunitXmlReader{
+
+    private final List<TestCaseElement> testCaseElementList;
+    private final String directoryPath;
+
+    public JunitXmlReader(String testReportDirectoryPath){
+        this.directoryPath = testReportDirectoryPath;
+        this.testCaseElementList = getTestCaseElementsFromFileSystem();
+
+    }
+
+    private List<TestCaseElement> getTestCaseElementsFromFileSystem() {
+        def listOfFiles = []
+        def fileContents = new File(this.directoryPath)
+        fileContents.eachFileRecurse(FileType.FILES) { file ->
+            listOfFiles << file
+
+        }
+
+        List<TestCaseElement> testCases = new ArrayList<>();
+
+        listOfFiles.stream().filter({file -> file.path.contains(".xml")}).each{ thefile ->
+            new XmlSlurper().parse(thefile.path).testsuite.testcase.each {
+
+                Map<String, String> attributeMap = new HashMap<>();
+                attributeMap.put("caseName", it.@name.toString());
+                attributeMap.put("suiteName", it.@classname.toString());
+                attributeMap.put("executionTime", it.@time.toString());
+                attributeMap.put("filePath", thefile.path.toString());
+
+                if (it.children().size() > 0){
+                    it.children().each{
+                        if (it.name() == "failure") {
+                            attributeMap.put("outcome", "fail");
+                        }
+                        else if (it.name() == "skipped") {
+                            attributeMap.put("outcome", "skip");
+                        }
+                    }
+                }
+                else {
+                    attributeMap.put("outcome", "pass")
+                }
+
+
+
+                testCases.add(new TestCaseElement(attributeMap));
+            }
+        }
+        return testCases;
+    }
+
+
+    public List<TestExecution> getTestExecutions(){
+        List<TestExecution> testExecutionList = new ArrayList<>();
+        this.testCaseElementList.each {testcase ->
+            boolean passed = testcase.testCaseAttributes.get("outcome").equals("pass");
+            boolean failed = testcase.testCaseAttributes.get("outcome").equals("fail");
+            boolean skipped = testcase.testCaseAttributes.get("outcome").equals("skip");
+
+            TestExecution testExecution = new TestExecution()
+                    .withIsFailed(failed)
+                    .withIsPassed(passed)
+                    .withIsSkipped(skipped)
+                    .withTestDuration(testcase.testCaseAttributes.get("executionTime"))
+                    .withTestName(testcase.testCaseAttributes.get("caseName"))
+                    .withTestGroup(testcase.testCaseAttributes.get("suiteName"))
+                    .withPathToJunitReport(testcase.testCaseAttributes.get("filePath"));
+            testExecutionList.add(testExecution);
+        }
+        return testExecutionList;
+    }
+
+}
+
+public class TestCaseElement{
+    public final Map<String, String> testCaseAttributes;
+
+    public TestCaseElement(Map<String, String> attributes){
+        this.testCaseAttributes = attributes;
+    }
+}
+
+public class TestExecution {
+
+
+    public String testName;
+    public String testGroup;
+    public String pathToJunitReport;
+    public String testDuration;
+    public Boolean isPassed;
+    public Boolean isFailed;
+    public Boolean isSkipped;
+
+    public TestExecution withTestName(String testName) {
+        this.testName = testName;
+        return this;
+    }
+
+    public TestExecution withTestGroup(String testGroup) {
+        this.testGroup = testGroup;
+        return this;
+    }
+
+    public TestExecution withPathToJunitReport(String pathToJunitReport) {
+        this.pathToJunitReport = pathToJunitReport;
+        return this;
+    }
+
+    public TestExecution withTestDuration(String testDuration) {
+        this.testDuration = testDuration;
+        return this;
+    }
+
+    public TestExecution withIsPassed(Boolean isPassed) {
+        this.isPassed = isPassed;
+        return this;
+    }
+
+    public TestExecution withIsFailed(Boolean isFailed) {
+        this.isFailed = isFailed;
+        return this;
+    }
+
+    public TestExecution withIsSkipped(Boolean isSkipped) {
+        this.isSkipped = isSkipped;
+        return this;
+    }
+
+}
---
+++ xmlSlurperTests.groovy
@@ -0,0 +1,25 @@
+import java.util.stream.Collectors
+
+lib = new jUnitReportXmlSlurper()
+reportPath = "/Users/kwaechter/results"
+tests = lib.getTestReportSummary("/Users/kwaechter/results/");
+
+println "Running Unit Tests..."
+
+//Check that failed tests are counted properly
+assert tests.testFailureCount == 4
+assert tests.failedTestList.size() ==4
+//Check that skipped tests are counted properly
+assert tests.testSkipCount == 2
+assert tests.skippedTestList.size() ==2
+//Check that passed tests are counted properly
+assert tests.testPassCount == 5
+assert tests.passedTestList.size() == 5
+//Test that list is filtering distinct paths: There are four failed tests but only 3 files with failures.
+assert tests.getFailedTestReportPaths().size() == 3
+//Check to see if the total executions equal expected amount
+assert tests.testExecutionList.size() == 11
+//Make sure sum of counts equal total list size
+assert tests.testFailureCount + tests.testPassCount + tests.testSkipCount == tests.testExecutionList.size()
+
+println "Finished!"
